f949a3cd38699715350fa63c2c0035703fc35bbf
Dan Safee <dan@danwins.com>
2021-12-22 17:12:50 -0500

    Update

--- src/Zonk.vue
+++ src/Zonk.vue
@@ -94,8 +94,8 @@ export default {
             var vm = this
             this.changeTeam(this.currentTeam)
             this.cards = []
-            
-            var cardPossibilities = [-1, -2, 100, 100, 200, 200, 200, 200, 300, 300, 300, 300, 400, 400, 400, 400, 500, 500, 500, 500, 0, 0, 0, 0, 0, 0, 0, 0]
+
+            var cardPossibilities = [-1, -2, 100, 200, 300, 300, 400, 400, 500, 500, 600, 600, 700, 700, 800, 800, 900, 900, 1000, 1000, 2000, 0, 0, 0, 0, 0, 0, 0]
 
             cardPossibilities = cardPossibilities.map(function (each, index) {
                 return {
