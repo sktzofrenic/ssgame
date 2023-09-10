dbdebc32938dc179e0edbd1afd98fbaf4111a4c0
Dan Safee <dan@getdrx.com>
2023-09-10 08:32:53 -0400

    Update build again

--- src/Zonk.vue
+++ src/Zonk.vue
@@ -96,7 +96,7 @@ export default {
             this.cards = []
 
             var cardPossibilities = [-1, -2, 100, 200, 200, 300, 300, 400, 400, 500, 500, 600, 600, 700, 700, 800,
-                800, 900, 900, 1000, 1000, 2000, 0, 0, 0, 0]
+                800, 900, 900, 1000, 1000, 2000, 0, 0, 0, 0, 100, 100]
 
             cardPossibilities = cardPossibilities.map(function (each, index) {
                 return {
